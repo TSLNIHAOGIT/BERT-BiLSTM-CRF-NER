ca0c9ddfaa44e0ec7532db7633c1096a6975e991
taoshilai <taoshilai@x3platform.com>
2019-04-21 22:35:37 +0800

    update convert nyt10

--- convert_data_to_nyt10/convert_to_nyt.py
+++ convert_data_to_nyt10/convert_to_nyt.py
@@ -1,33 +1,64 @@
-path='../NERdata/dev_ch.txt'
+path='../output/label_test.txt'
 with open(path,encoding='utf8') as f:
     each_point_text=[]
     each_point_label=[]
+    right_amounts=0
+
+
+    each_sentence_entitys = []
+    eachs_entity = ''
 
     for each in f:
-        each_entity=[]
+
+
         each_list=each.strip().split(' ')
 
         length=len(each_list)
         if length<2:
+            if len(each_sentence_entitys)>0:
+                 print('each_sentence_entitys',each_sentence_entitys)
+                 print('\n*********************\n')
             print(' '.join(each_point_text))
             print(' '.join(each_point_label))
             each_point_text=[]
             each_point_label=[]
-            print('\n*********************\n')
+            each_sentence_entitys = []
+            # print("right_amounts",right_amounts)
+
         else:
             # print('each_list', each_list)
             text = each_list[0]
             true_label = each_list[1]
+            predicted_label=each_list[2]
+
             each_point_text.append(text)
             each_point_label.append(true_label)
-            ll=each_point_label.split('-')
+
+            # if true_label==predicted_label and true_label!='O':
+            #     right_amounts=right_amounts+1
+            #     print("right_amounts",right_amounts)
+            #     print('each_list', each_list)
+
+
+            ll=true_label.split('-')
+            # print('ll',ll)
             if len(ll)<2:
                 continue
             else:
-                
-                if ll[0]=='B':
-                    each_entity.append(each_point_text)
-            # if each_point_label in ['B-PER','I-PER','B-LOC','I-LOC','B-ORG','I-ORG']:
+                if ll[0]=='S':
+                    each_sentence_entitys.append(text)
+                elif ll[0]=='B':
+                    eachs_entity=eachs_entity+' ' +text
+                    pass
+                elif ll[0]=='I':
+                    eachs_entity = eachs_entity+' ' + text
+                    pass
+                elif ll[0]=='E':
+                    eachs_entity = eachs_entity+' '  + text
+                    each_sentence_entitys.append(eachs_entity)
+                    # print('each_sentence_entitys',each_sentence_entitys)
+                    eachs_entity=''
+                    pass
+                else:
+                    continue
 
-
-            # print('each list',each_list)
